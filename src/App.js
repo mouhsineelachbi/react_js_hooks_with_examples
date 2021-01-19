2423bd89086f5bac6070526933c2425317cf1ba5
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-01-19 23:51:23 +0100

    useDebugValue is valuable when builing a custom hook and use react dev tools to debug It

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useDebugValue, useState, useEffect } from 'react';
 import './App.css';
 
+//  useDebugValues is useful when building a custom hook
+//  and used to debug in react dev tools
+
+function fetchFromDatabase(userId) {
+  return {displayName: "Mouhsine"}
+}
+
+
+function useDisplayName () { // <= this is our custom hook
+  const [displayName, setDisplayName] = useState();
+
+  useEffect(()=>{
+    // just an example of fetching data not real
+    const userId = 'kdkdk'
+    const data = fetchFromDatabase(userId);
+    setDisplayName(data.displayName);
+  },[])
+
+  useDebugValue(displayName ?? 'loading...')
+
+  return displayName;
+}
+
 function App() {
+
+  const displayName = useDisplayName();
+
   return (
     <div className="App">
-            
+      <button>{ displayName }</button>            
     </div>
   );
 }
