dd5be5ef88acd1c6524e466a84e9cbb65034db62
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-01-19 22:48:01 +0100

    useCallback to memorize functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
+//  useCallback is used to memorize an entier function
+
 function App() {
+  const [count, setCount] = useState(60);
+
+  const memoizedCallback  = useCallback(() => {
+    alert(`Count ${count}`);
+  }, [count]) // Dependency array variables to re-render the children component
   return (
     <div className="App">
-            
+      <button onClick={()=>setCount(10)}>Change</button>
+      <SomeChild handler={memoizedCallback} />
     </div>
   );
+}
+
+function SomeChild (handler) {
+  return (
+    <>
+    </>
+  )
 }
 
 export default App;
