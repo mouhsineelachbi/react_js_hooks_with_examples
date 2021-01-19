ca390816e91342bbc717b55167acd55ebba4c7e2
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-01-19 23:36:51 +0100

    useLayoutEffect similar to useEffect but Its Callback function will run after rendering and before painting to the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import './App.css';
 
+//  useEffectLayout is similar to use effect but the difference is :
+//  the callback (function)run after rendering but before painting to screen
+//  That means that react will wait your code 
+//  to finish running before updating the UI for the end user
+
+
 function App() {
+
+  const myBtn = useRef(null);
+
+  useLayoutEffect(()=>{
+    const rect = myBtn.current.getBoundingClientRect();
+    console.log(rect.height);
+  })
+
   return (
     <div className="App">
-            
+      <button ref={myBtn}></button>        
     </div>
   );
 }
